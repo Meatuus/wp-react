8ca8fffb83008d7aa566f534ed4210c6ad26c7f7
Tyler <tcameronsmith@gmail.com>
2017-11-10 15:10:54 +1100

    create Project component, add state for hover for project

--- wp-react/src/containers/Projects/Projects.js
+++ wp-react/src/containers/Projects/Projects.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class Projects extends Component {
+import Project from '../../components/Project/Project';
 
+class Projects extends Component {
     render() {
         const {projects} = this.props
-
         
         const hero = projects.map(proj => {
-            let bg = {
-                background: `url(${proj.acf.feature_image.url})`,
-                backgroundPosition: 'center',
-                backgroundSize: 'cover' 
-            }
-            return <div key={proj.id} className="container" style={bg}>
-                {/* <div className="proj-thmb__img-wrap" >
-                    <img className="proj-thmb__img" src={proj.acf.feature_image.url} alt={proj.acf.feature_image.title} />
-                </div> */}
-                <div className="titles">
-                    <h2 className="h2">{proj.title.rendered}</h2>
-                    <h3 className="h3">{proj.acf.subheading}</h3>
-                </div>
-                <a href="" className="link">Learn More</a>
-            </div>
+            return <Project project={proj} key={proj.id} />
         })
 
         return (
             <div>
-                <h1 className="proj-thmbs__title">Projects</h1>
+                <h1 className="proj-thmbs__title" onMouseEnter={this.hovered}>Projects</h1>
                 <div id="projects" className="proj-thmbs">
                     {hero}
                 </div>
