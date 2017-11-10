bfd59d5e8eb09c04f6d422b01d89f3fa526d0bf1
Tyler <tcameronsmith@gmail.com>
2017-11-10 13:02:30 +1100

    move projects to it's own folder

--- wp-react/src/containers/Projects/Projects.js
+++ wp-react/src/containers/Projects/Projects.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
+import './styles.css';
 
 class Projects extends Component {
 
     render() {
         const {projects} = this.props
 
+        
         const hero = projects.map(proj => {
-            return <div key={proj.id} className="proj-thmb__container">
-                <div className="proj-thmb__img-wrap">
+            let bg = {
+                background: `url(${proj.acf.feature_image.url})`,
+                backgroundPosition: 'center',
+                backgroundSize: 'cover' 
+            }
+            return <div key={proj.id} className="container" style={bg}>
+                {/* <div className="proj-thmb__img-wrap" >
                     <img className="proj-thmb__img" src={proj.acf.feature_image.url} alt={proj.acf.feature_image.title} />
+                </div> */}
+                <div className="titles">
+                    <h2 className="h2">{proj.title.rendered}</h2>
+                    <h3 className="h3">{proj.acf.subheading}</h3>
                 </div>
-                <h1 className="proj-thmb__title">{proj.title.rendered}</h1>
-                <p className="proj-thmb__subheading">{proj.acf.subheading}</p>
+                <a href="" className="link">Learn More</a>
             </div>
         })
 
