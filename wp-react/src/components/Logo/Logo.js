5927d96faf12bc7a2c1775f99ae13bcd434dbf3b
Tyler <tcameronsmith@gmail.com>
2017-11-20 09:32:31 +1100

    remove nav wrap and add link around text

--- wp-react/src/components/Logo/Logo.js
+++ wp-react/src/components/Logo/Logo.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Typist from 'react-typist';
 import './style.css';
 
 const Logo = (props) => {
     const { title } = props
     return (
-        <nav>
-            <h2 className="nav-title">
-                &#123;&nbsp;
-                <Typist className="nav-span" cursor={{ show: true, hideWhenDone: true, blink: true, element: '|', hideWhenDoneDelay: 3000 }} avgTypingDelay={250} startDelay={2000} >
-                    {title}
-                </Typist>
-                &nbsp;&#125;
-            </h2>
-        </nav>
+        <h1 className="nav-title">
+            &#123;&nbsp;
+            <Link to="/">
+            <Typist className="nav-span" cursor={{ show: true, hideWhenDone: true, blink: true, element: '|', hideWhenDoneDelay: 3000 }} avgTypingDelay={250} startDelay={2000} >
+                {title}
+            </Typist>
+            </Link>
+            &nbsp;&#125;
+        </h1>
     )
 }
 
